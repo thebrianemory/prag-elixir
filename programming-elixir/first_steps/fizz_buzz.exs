eef14bc3637bf65390fbb1ea8c6688e49a41ceed
thebrianemory <the@brianemory.com>
2017-06-01 22:12:19 -0700

    Update fizzbuzz

--- programming-elixir/first_steps/fizz_buzz.exs
+++ programming-elixir/first_steps/fizz_buzz.exs
@@ -5,19 +5,8 @@ fizz_buzz = fn
   _, _, n -> n
 end
 
-fizz_buzz.(0,1,1)
-fizz_buzz.(0,0,1)
-fizz_buzz.(1,0,1)
-fizz_buzz.(1,1,1)
-
 fizzy = fn n ->
   fizz_buzz.(rem(n,3), rem(n,5), n)
 end
 
-fizzy.(10)
-fizzy.(11)
-fizzy.(12)
-fizzy.(13)
-fizzy.(14)
-fizzy.(15)
-fizzy.(16)
+fb = for x <- (1..100), do: fizzy.(x)
