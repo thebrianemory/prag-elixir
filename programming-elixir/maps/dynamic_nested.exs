9d9a5c810c98c8a975eeca7ead6376d5685aeffd
thebrianemory <the@brianemory.com>
2017-06-17 22:28:47 -0700

    Nested dictionary structures

---
+++ programming-elixir/maps/dynamic_nested.exs
@@ -0,0 +1,16 @@
+nested = %{
+  buttercup: %{
+    actor: %{
+      first: "Robin",
+      last: "Wright"
+    },
+    role: "princess"
+  },
+  westley: %{
+    actor: %{
+      first: "Cary",
+      last: "Ewles"
+    },
+    role: "farm boy"
+  }
+}
